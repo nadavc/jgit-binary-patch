eclipse-jgit/jgit

Implement support for adding binary files in ApplyCommand
Binary patches in Git were designed to be transferred via email, so they
are compressed and custom-base85 encoded in the patch file. This patch
implements support for applying new literal binary patches in JGit.

Change-Id: I36e392346ee8342b75d9547d9bb75d2d805ad5e1
Signed-off-by: Nadav Cohen <nadavcoh@gmail.com>

org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/zlib/ZlibSupport.java
@@ -1,0 +1,77 @@
+/*
+ * Copyright (C) 2016, Nadav Cohen <nadavcoh@gmail.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.internal.storage.zlib;
+
+import java.io.ByteArrayOutputStream;
+import java.util.zip.DataFormatException;
+import java.util.zip.InflaterOutputStream;
+
+/**
+ * Utility class to inflate zlib
+ */
+public class ZlibSupport {
+
+	/**
+	 * Inflates a zlib byte array
+	 *
+	 * @param bytes
+	 *            zlib-compressed bytes
+	 * @param expectedSize
+	 *            expected result size
+	 * @return inflated bytes
+	 * @throws DataFormatException
+	 */
+	public static byte[] inflate(byte[] bytes, int expectedSize)
+			throws DataFormatException {
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream(expectedSize);
+			InflaterOutputStream ios = new InflaterOutputStream(baos);
+			ios.write(bytes, 0, bytes.length);
+			return baos.toByteArray();
+		} catch (Exception e) {
+			throw new DataFormatException();
+		}
+	}
+
+}

nadavc/jgit-binary-patch

org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/zlib/ZlibSupport.java
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2016, Nadav Cohen <nadavcoh@gmail.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.internal.storage.zlib;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.DataFormatException;
+import java.util.zip.InflaterOutputStream;
+
+/**
+ * Utility class to inflate zlib
+ */
+public class ZlibSupport {
+
+	/**
+	 * Inflates a zlib byte array
+	 *
+	 * @param bytes
+	 *            zlib-compressed bytes
+	 * @param expectedSize
+	 *            expected result size
+	 * @return inflated bytes
+	 * @throws DataFormatException
+	 */
+	public static byte[] inflate(byte[] bytes, int expectedSize)
+			throws DataFormatException {
+
+		try (ByteArrayOutputStream baos = new ByteArrayOutputStream(expectedSize);
+			 InflaterOutputStream ios = new InflaterOutputStream(baos)) {
+			ios.write(bytes, 0, bytes.length);
+			return baos.toByteArray();
+		} catch (IOException e) {
+			throw new DataFormatException();
+		}
+	}
+
+}